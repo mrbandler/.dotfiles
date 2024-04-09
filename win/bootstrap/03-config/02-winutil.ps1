eef4802a093e3cc79da44e30e37bb4aeaafd5cc3
mrbandler <me@mrbandler.dev>
2024-04-09 20:45:58 +0000

    chore(winutil): Remove configuration and added script to just launch the tool

--- win/bootstrap/03-config/02-winutil.ps1
+++
@@ -1,4 +0,0 @@
-# TODO: Look at the parts that I need in isolation and see if I can use them in my own scripts
-# Write-Host "Applying winutil configuration..."
-# $configPath = Join-Path -Path $PSScriptRoot -ChildPath "../../winutil.json"
-# &([ScriptBlock]::Create((Invoke-RestMethod https://christitus.com/win))) -Config $configPath -Run
