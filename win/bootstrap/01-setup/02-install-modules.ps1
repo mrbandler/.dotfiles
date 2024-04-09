05aa699a7fcdf9ceab042e6f9407869a7b106889
mrbandler <me@mrbandler.dev>
2024-04-09 21:46:16 +0000

    chore(setup): Moved modules and features to configuration

--- win/bootstrap/01-setup/02-install-modules.ps1
+++ win/bootstrap/01-setup/02-install-modules.ps1
@@ -1,23 +1,26 @@
-$modulesToInstall = @(
-    'PSDotFiles',
-    'winget-install',
-    'powershell-yaml',
-    'Terminal-Icons'
-)
-
 Write-Host "Configuring repos..."
 Install-PackageProvider -Name "NuGet" -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
+Write-Host "Installing YAML module to read configuration..."
+Install-Module -Name "powershell-yaml" -Scope CurrentUser -SkipPublisherCheck
+
+Write-Host "Reading configuration..."
+Import-Module powershell-yaml
+$configPath = Join-Path -Path $PSScriptRoot -ChildPath "../../config.yml"
+$config = Get-Content $configPath -Raw | ConvertFrom-Yaml
+$modules = $config.modules
+
 Write-Host "Installing modules..."
-foreach ($module in $modulesToInstall) {
+foreach ($module in $modules) {
     if (-not (Get-Module -ListAvailable -Name $module)) {
-        Install-Module -Name $module -Scope CurrentUser -SkipPublisherCheck
-        Write-Host "Installed module: $module"
+        Write-Host "Installing module '$($modulen.name)'"
+        Install-Module -Name $module.name -Scope CurrentUser -SkipPublisherCheck
+        if ($module.install) {
+            Invoke-Expression $module.install
+        }
     }
     else {
-        Write-Host "Module already installed: $module"
+        Write-Host "Module '$($module.name)' already installed"
     }
 }
-
-Install-Script winget-install -Force
---
+++ win/bootstrap/01-setup/04-features.ps1
@@ -0,0 +1,11 @@
+Write-Host "Reading configuration..."
+Import-Module powershell-yaml
+$configPath = Join-Path -Path $PSScriptRoot -ChildPath "../../config.yml"
+$config = Get-Content $configPath -Raw | ConvertFrom-Yaml
+$features = $config.features
+
+Write-Host "Enabling features..."
+foreach ($feature in $features) {
+    Write-Host "Enabling '$feature' feature..."
+    Enable-WindowsOptionalFeature -Online -FeatureName $feature -All -NoRestart
+}
