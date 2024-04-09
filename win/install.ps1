f2cf6227867141ecc87293ca5926b1ab3350b7e4
mrbandler <me@mrbandler.dev>
2024-04-09 22:01:29 +0000

    chore(install): Added bypass policy for process for bootstrap execution

--- win/install.ps1
+++ win/install.ps1
@@ -26,6 +26,7 @@ Remove-Item -Path $zipPath -Force
 Remove-Item -Path $tempDir -Recurse -Force
 
 Set-Location -Path $dotfilesDir
+Set-ExecutionPolicy Bypass -Scope Process -Force
 & ".\win\bootstrap.ps1"
 
 git init
